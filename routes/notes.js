805a39c5a09b714aef977bef1c54a5e4b891555e
Shawn Crook <shawncrook411@gmail.com>
2023-11-14 23:04:22 -0700

    added delete route

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,8 @@
 const path = require('path')
 const notes = require ('express').Router()
 const db = path.join(__dirname, '../db/db.json')
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const {readAndAppend} = require('../helpers/fsUtils');
 const fs = require('fs')
-
 
 notes.get('/', (req, res) => {
     console.log('landed on notes')
@@ -15,5 +14,30 @@ notes.post('/', (req, res) => {
     res.status(200).send("Good!")
 })
 
+notes.delete('/:title', (req, res) => {
+    let notesData
+    fs.readFile(db, (error, data) => {
+        if (error)
+        {
+            console.error(error)
+        }
+        else
+        {
+            notesData = JSON.parse(data)      
+
+            for (let note of notesData)
+            {
+                if (note.title === req.params.title)
+                {
+                    notesData.splice(notesData.indexOf(note), 1)
+                    notesData = JSON.stringify(notesData, null, 4)
+                    fs.writeFile(db, notesData, (error) => error ? console.error(error) : console.log("Success"))
+                    res.status(200).send("Successfully deleted note")
+                    return
+                }
+            }
+        }        
+    })    
+})
 
 module.exports = notes;
